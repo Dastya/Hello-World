6463894600971cb311561fbacf6cc4e99866de25
Dastya <daniillipenec@gmail.com>
2022-11-29 16:09:32 +0200

    changed main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.net.SocketPermission;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello mates!");
+        System.out.println("Hello Mates!");
     }
 }
